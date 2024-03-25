6ed5ee78ff0f0a96e79f0181cb7d923cc6323002
kimsejas <sejaskm@gmail.com>
2024-03-25 17:55:55 -0400

    fixed tower conflicts

--- src/tower.cpp
+++ src/tower.cpp
@@ -2,29 +2,29 @@
 #include "tower.h"
 #include "tower_gui.h"
 
-Tower::Tower(std::string name, int health, int damage, int range, int areaOfEffect, DamageType damageType, const TowerLocation& location)
-    : name(name), health(health), damage(damage), range(range), areaOfEffect(areaOfEffect), damageType(damageType), location(location) {}
+Tower::Tower(std::string name, int health, int damage, int range, int areaOfEffect, DamageType damageType, const TowerLocation& location, int buildCost)
+    : name(name), health(health), damage(damage), range(range), areaOfEffect(areaOfEffect), damageType(damageType), location(location), buildCost(buildCost) {}
 
 Tower::~Tower() {}
 
-LaserTower::LaserTower(std::string name, int health, int damage, int range, int areaOfEffect, DamageType damageType, const TowerLocation& location, int fireRate)
-    : Tower(name, health, damage, range, areaOfEffect, damageType, location), fireRate(fireRate) {
+LaserTower::LaserTower(std::string name, int health, int damage, int range, int areaOfEffect, DamageType damageType, const TowerLocation& location, int fireRate, int buildCost)
+    : Tower(name, health, damage, range, areaOfEffect, damageType, location, buildCost), fireRate(fireRate) {
 }
 
 void LaserTower::attack() {
     // TODO
 }
 
-BombTower::BombTower(std::string name, int health, int damage, int range, int areaOfEffect, DamageType damageType, const TowerLocation& location, int fireRate)
-    : Tower(name, health, damage, range, areaOfEffect, damageType, location), fireRate(fireRate) {
+BombTower::BombTower(std::string name, int health, int damage, int range, int areaOfEffect, DamageType damageType, const TowerLocation& location, int fireRate, int buildCost)
+    : Tower(name, health, damage, range, areaOfEffect, damageType, location, buildCost), fireRate(fireRate) {
 }
 
 void BombTower::attack() {
     // TODO
 }
 
-Barracks::Barracks(std::string name, int health, int damage, int range, int areaOfEffect, DamageType damageType, const TowerLocation& location)
-    : Tower(name, health, damage, range, areaOfEffect, damageType, location) {
+Barracks::Barracks(std::string name, int health, int damage, int range, int areaOfEffect, DamageType damageType, const TowerLocation& location, int buildCost)
+    : Tower(name, health, damage, range, areaOfEffect, damageType, location, buildCost) {
 }
 
 void Barracks::attack() {
@@ -32,12 +32,13 @@ void Barracks::attack() {
 }
 
 Tower* Tower::createTower(const std::string& type, const TowerLocation& location) {
+    //TODO MAKE TOWER COSTS AND FIRE RATE CONSTANTS + CHANGE THEM 
     if (type == "Barracks") {
-        return new Barracks("Barracks", 100, 100, 0, 0, DamageType::NORMAL, location);
+        return new Barracks("Barracks", 100, 100, 0, 0, DamageType::NORMAL, location, 50);
     } else if (type == "Bomb") {
-        return new BombTower("BombTower", 100, 100, 100, 100, DamageType::BOMB, location, 1);
+        return new BombTower("BombTower", 100, 100, 100, 100, DamageType::BOMB, location, 10, 50);
     } else if (type == "Laser") {
-        return new LaserTower("LaserTower", 100, 100, 100, 0, DamageType::LASER, location, 1);
+        return new LaserTower("LaserTower", 100, 100, 100, 0, DamageType::LASER, location, 10, 50);
     }
     return nullptr;
 }
