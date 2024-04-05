e8fb48b63155db477f3bd10c88d8fc643a3c9064
hmzimmerman <hmzimmerman@wm.edu>
2024-04-04 22:53:33 -0400

    Implemented basic enemy targetting

--- include/barracks.h
+++ include/barracks.h
@@ -7,14 +7,19 @@
 class Barracks : public Tower {
     private:
     std::vector<std::pair<int, int>> soldierLocations;
-    std::vector<std::pair<int, int>> enemyStoppingLocations;
+    //std::vector<std::pair<int, int>> enemyStoppingLocations;
 
+    Enemy* target;
     View* view;
 
 public:
     Barracks(std::string name, int health, int damage, int range, DamageType damageType, const TowerLocation& location, int buildCost, View* view);
 
     void attack() override;
+
+    void updateTarget(std::vector<Enemy>& enemies) override;
+
+    bool isEnemyNearSoldier(const Enemy& enemy);
 
     // Getters
     const std::vector<std::pair<int, int>>& getSoldierLocations() const;
--- src/barracks.cpp
+++ src/barracks.cpp
@@ -10,7 +10,40 @@ Barracks::Barracks(std::string name, int health, int damage, int range, DamageTy
     }) {}
 
 void Barracks::attack() {
+    // Check if the tower has a target and the target is alive
+    if (target && target->isAlive()) {
+        std::cout << "Halting enemy: " << target->getName() << std::endl;
+
+        // Reset the target after attacking?
+        //target = nullptr;
+    }
     view->renderSoldiers();
+}
+
+void Barracks::updateTarget(std::vector<Enemy>& enemies) {
+    // Find the first enemy near a soldier
+    for (const auto& enemy : enemies) {
+        if (isEnemyNearSoldier(enemy)) {
+            target = const_cast<Enemy*>(&enemy);
+            return;
+        }
+    }
+    // If no enemy is found near a soldier, set target to none
+    target = nullptr;
+}
+
+bool Barracks::isEnemyNearSoldier(const Enemy& enemy) {
+    for (const auto& soldierLocation : soldierLocations) {
+        // Check if the enemy is within 20 pixels north or west of the soldiers
+        // TODO: Double check area bounds
+        if ((enemy.getX() >= soldierLocation.first - 30) && 
+            (enemy.getX() <= soldierLocation.first + 80) && 
+            (enemy.getY() >= soldierLocation.second - 30) && 
+            (enemy.getY() <= soldierLocation.second + 80)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 const std::vector<std::pair<int, int>>& Barracks::getSoldierLocations() const {
