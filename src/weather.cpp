a12f8d4411e8500e27a9ddb6183887cbe92cb932
JonathonWest <jbwest02@wm.edu>
2024-04-18 23:53:46 -0400

    basic setup for weather files

--- src/weather.cpp
+++ src/weather.cpp
@@ -3,3 +3,60 @@
 
 #include "weather.h"
 
+
+Weather::Weather(){
+    towerHpMod = 0;
+    towerRangeMod = 0;
+    enemyHpMod = 0;
+    enemySpeedMod = 0;
+}
+
+void Weather::weatherSetAcidRain(){
+    towerHpMod = 2;
+    towerRangeMod = 0;
+    enemyHpMod = 0;
+    enemySpeedMod = 0;
+}
+
+void Weather::weatherSetSandstorm(){
+    towerHpMod = 0;
+    towerRangeMod = 2;
+    enemyHpMod = 0;
+    enemySpeedMod = 0;
+}
+
+void Weather::weatherSetRadiation(){
+    towerHpMod = 0;
+    towerRangeMod = 0;
+    enemyHpMod = 2;
+    enemySpeedMod = 0;
+}
+
+void Weather::weatherSetEarthquake(){
+    towerHpMod = 3;
+    towerRangeMod = 0;
+    enemyHpMod = 0;
+    enemySpeedMod = 2;
+}
+
+void Weather::weatherSetWind(){
+
+}
+
+
+
+float Weather::getTowerHpMod(){
+    return towerHpMod;
+}
+
+float Weather::getTowerRangeMod(){
+    return towerRangeMod;
+}
+
+float Weather::getenemyHpMod(){
+    return enemyHpMod;
+}
+
+float Weather::getenemySpeedMod(){
+    return enemySpeedMod;
+}
