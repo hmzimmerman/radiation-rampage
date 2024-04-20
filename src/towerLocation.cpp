bc0b985d560603afdac4a9305cd178f781bb6bed
hmzimmerman <hmzimmerman@wm.edu>
2024-04-19 23:47:09 -0400

    Fixed bug to resume halted enemy after selling barracks tower

--- src/towerLocation.cpp
+++ src/towerLocation.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include "towerLocation.h"
 #include "tower.h"
+#include "barracks.h"
 
 std::vector<TowerLocation> TowerLocationManager::towerLocations = {
     TowerLocation(275, 71),
@@ -17,6 +18,11 @@ std::vector<TowerLocation>& TowerLocationManager::getTowerLocations() {
 void TowerLocationManager::removeTower(const Tower& tower) {
     for (size_t i = 0; i < towerLocations.size(); ++i) {
         if (towerLocations[i].tower.get() == &tower) {
+            // If barracks tower, resume halted enemy before removing
+            if (towerLocations[i].tower->getName() == "Barracks") {
+                dynamic_cast<Barracks*>(towerLocations[i].tower.get())->resumeEnemyMovement();
+            }
+
             towerLocations[i].tower = nullptr;
             towerLocations[i].occupied = false;
             break;
